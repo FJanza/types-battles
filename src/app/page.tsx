20615939f7bd284b3357d6344ef62c33b7fd6c10
FJanza <franciscojanza001@gmail.com>
2024-05-25 19:19:12 -0300

    <feat>[dev] first prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,215 @@
-import Image from "next/image";
+"use client";
+import {useEffect, useRef, useState} from "react";
+import {WORDS} from "src/utils/constants";
 
 export default function Home() {
+  const INITIAL_TIME = 5;
+
+  const $paragraph = useRef<HTMLParagraphElement>(null);
+  const $input = useRef<HTMLInputElement>(null);
+  const $time = useRef<HTMLTimeElement>(null);
+
+  const [gameOverState, setGameOverState] = useState(false);
+  const [gameOverData, setGameOverData] = useState<{
+    wpm: number;
+    accuracy: string;
+  }>();
+
+  let words: any = [];
+
+  words = WORDS;
+
+  const [paragraph] = useState<string | TrustedHTML>(
+    words
+      .map((word: string, index: number) => {
+        const letters = word.split("");
+        return `<tb-word ${index === 0 ? "class=active" : ""}>${letters
+          .map(
+            (letter: string, indexLetter: number) =>
+              `<tb-letter ${
+                index === 0 && indexLetter === 0 ? "class=active" : ""
+              }>${letter}</tb-letter>`
+          )
+          .join("")}</tb-word>`;
+      })
+      .join("")
+  );
+
+  function initGame() {
+    setGameOverState(false);
+    let currentTime = INITIAL_TIME;
+
+    const intervalId = setInterval(() => {
+      currentTime--;
+      if ($time.current) {
+        $time.current.textContent = String(currentTime);
+
+        if (currentTime === 0) {
+          clearInterval(intervalId);
+          gameOver();
+        }
+      }
+    }, 1000);
+  }
+
+  function initEvents() {
+    //TODO si no hay eventos borrar este metodo
+  }
+
+  function onKeyDown(event: any) {
+    $input.current?.focus();
+
+    const $currentWord = $paragraph.current!.querySelector("tb-word.active");
+    const $currentLetter = $currentWord!.querySelector("tb-letter.active");
+
+    const {key} = event;
+    if (key === " ") {
+      event.preventDefault();
+
+      const $nextWord = $currentWord!.nextElementSibling;
+      const $nextLetter = $nextWord!.querySelector("tb-letter");
+
+      $currentWord!.classList.remove("active", "marked");
+      $currentLetter!.classList.remove("active");
+
+      $nextWord!.classList.add("active");
+      $nextLetter!.classList.add("active");
+
+      $input.current!.value = "";
+
+      const hasMissedLetters =
+        $currentWord!.querySelectorAll("tb-letter:not(.correct)").length > 0;
+
+      const classToAdd = hasMissedLetters ? "marked" : "correct";
+
+      $currentWord!.classList.add(classToAdd);
+      return;
+    }
+    if (key === "Backspace") {
+      const $prevWord = $currentWord!.previousElementSibling;
+      const $prevLetter = $currentLetter?.previousElementSibling;
+
+      if (!$prevWord && !$prevLetter) {
+        event.preventDefault();
+        return;
+      }
+      const $wordMarked = $paragraph.current!.querySelector("tb-word.marked");
+      if ($wordMarked && !$prevLetter) {
+        event.preventDefault();
+
+        $prevWord?.classList.remove("marked");
+        $prevWord?.classList.add("active");
+
+        const $letterToGo = $prevWord?.querySelector("tb-letter:last-child");
+
+        $currentLetter?.classList.remove("active");
+        $letterToGo?.classList.add("active");
+
+        $input.current!.value = [
+          ...$prevWord!.querySelectorAll(
+            "tb-letter.correct, tb-letter.incorrect"
+          ),
+        ]
+          .map(($el) => {
+            return $el.classList.contains("correct") ? $el.textContent : "+";
+          })
+          .join("");
+      }
+    }
+  }
+
+  function onKeyUp() {
+    const $currentWord = $paragraph.current!.querySelector("tb-word.active");
+    const $currentLetter =
+      $paragraph.current!.querySelector("tb-letter.active");
+
+    const currentWord = $currentWord!.textContent!.trim();
+
+    $input.current?.setAttribute("maxLength", String(currentWord?.length));
+
+    const $allLetters = $currentWord!.querySelectorAll("tb-letter");
+
+    $allLetters.forEach(($letter) =>
+      $letter.classList.remove("correct", "incorrect")
+    );
+
+    $input.current?.value.split("").forEach((char, index) => {
+      const $letter = $allLetters[index];
+      const letterToCheck = currentWord[index];
+
+      const isCorrect = char === letterToCheck;
+
+      const letterClass = isCorrect ? "correct" : "incorrect";
+
+      $letter?.classList.add(letterClass);
+    });
+
+    $currentLetter?.classList.remove("active", "is-last");
+
+    const inputLength = $input.current!.value.length;
+
+    const $nextActiveLetter = $allLetters[inputLength];
+
+    if ($nextActiveLetter) {
+      $nextActiveLetter.classList.add("active");
+    } else {
+      $currentLetter!.classList.add("active", "is-last");
+
+      //TODO si no hay proxima palabra game over
+    }
+  }
+
+  function gameOver() {
+    const correctWords =
+      $paragraph.current!.querySelectorAll("tb-word.correct").length;
+    const correctLetter =
+      $paragraph.current!.querySelectorAll("tb-letter.correct").length;
+    const incorrectLetter = $paragraph.current!.querySelectorAll(
+      "tb-letter.incorrect"
+    ).length;
+
+    const totalLetters = correctLetter + incorrectLetter;
+
+    const accuracy =
+      totalLetters > 0 ? (correctLetter / totalLetters) * 100 : 0;
+
+    const wpm = (correctWords * 60) / INITIAL_TIME;
+    console.log({wpm, accuracy: accuracy.toFixed(2)});
+
+    setGameOverData({wpm, accuracy: accuracy.toFixed(2)});
+    setGameOverState(true);
+  }
+
+  useEffect(() => {
+    initGame();
+
+    initEvents();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {!gameOverState ? (
+        <section>
+          <input
+            autoFocus
+            ref={$input}
+            onKeyDown={onKeyDown}
+            onKeyUp={onKeyUp}
+          />
+          <p dangerouslySetInnerHTML={{__html: paragraph}} ref={$paragraph}></p>
+          <time ref={$time}></time>
+        </section>
+      ) : (
+        <section>
+          <h2>WPM</h2>
+          <h3>{gameOverData?.wpm}</h3>
+          <h2>Accuracy</h2>
+          <h3>{gameOverData?.accuracy}%</h3>
+          <button onClick={() => initGame()}>retry</button>
+        </section>
+      )}
     </main>
   );
 }
---
+++ src/components/typeBox/TypeBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TypeBox = () => {
+  return <div>TypeBox</div>;
+};
+
+export default TypeBox;
