20859bf6412ac56943aa832d15725e94610d8dfe
fjanza <franciscojanza001@gmail.com>
2025-04-21 10:00:59 -0300

    <feat> add new loader

---
+++ src/components/LoadingTyping/Index.tsx
@@ -0,0 +1,229 @@
+import React, {useState, useEffect, useRef} from "react";
+
+interface TeclaProps {
+  tecla: string;
+}
+
+const Tecla = ({tecla}: TeclaProps) => {
+  return (
+    <button
+      data-key={tecla}
+      className={`pointer-events-none bg-gray-200 border border-gray-300 text-gray-700 py-2 px-4 rounded shadow cursor-pointer transition-transform duration-100 ease-in-out
+        ${tecla === "MAYÚS" ? "w-32" : ""}
+        ${tecla === " " ? "w-full" : ""}
+        ${tecla === "  " ? "w-44 bg-transparent shadow-none" : ""}
+      `}
+    >
+      {tecla}
+    </button>
+  );
+};
+
+const EnterTecla = () => {
+  return (
+    <button
+      data-key="ENTER"
+      className="pointer-events-none absolute bg-gray-200 border border-gray-300 text-gray-700 rounded shadow cursor-pointer transition-transform duration-100 ease-in-out w-20 h-[142px] flex items-center justify-center"
+      style={{bottom: "154px", right: "-2px"}} // Ajusta la posición según sea necesario porque esta absolutizada
+    >
+      ENTER
+    </button>
+  );
+};
+
+const LoadingTyping = () => {
+  const teclasData: string[][] = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
+    ["MAYÚS", "Z", "X", "C", "V", "B", "N", "M", ",", "."],
+    ["  ", " ", "  "],
+  ];
+
+  const frases: string[] = [
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "type battle",
+    "click battle",
+  ];
+  const [fraseActualIndex, setFraseActualIndex] = useState<number>(0);
+  const [letraActualIndex, setLetraActualIndex] = useState<number>(0);
+  const [textoEscrito, setTextoEscrito] = useState<string>("");
+  const intervaloEntreLetras: number = 200;
+  const intervaloEntreFrases: number = 1000;
+  const animationTimeout = useRef<NodeJS.Timeout | null>(null);
+  const fraseTimeout = useRef<NodeJS.Timeout | null>(null);
+  const initialTimeoutRef = useRef<NodeJS.Timeout | null>(null); // Ref para el timeout inicial
+  const tecladoContainerRef = useRef<HTMLDivElement | null>(null); // Referencia al contenedor del teclado
+
+  const animarTecla = (key: string): void => {
+    const teclaElement = document.querySelector<HTMLButtonElement>(
+      `[data-key="${key.toUpperCase()}"]`
+    );
+    if (teclaElement) {
+      teclaElement.classList.add("animate-presionarTecla");
+      setTimeout(() => {
+        teclaElement.classList.remove("animate-presionarTecla");
+      }, 300);
+    }
+    setTextoEscrito((prevTexto) => prevTexto + key);
+  };
+
+  const animarEnter = (): void => {
+    const enterElement =
+      document.querySelector<HTMLButtonElement>(`[data-key="ENTER"]`);
+    if (enterElement) {
+      enterElement.classList.add("animate-presionarTecla");
+      setTimeout(() => {
+        enterElement.classList.remove("animate-presionarTecla");
+      }, 300);
+    }
+  };
+
+  const teclearFrase = (): void => {
+    const fraseActual = frases[fraseActualIndex];
+    const letraActual = fraseActual[letraActualIndex];
+
+    if (letraActual) {
+      const textoEscritoElement = document.getElementById("texto-escrito");
+      if (textoEscritoElement) {
+        textoEscritoElement.style.color =
+          letraActualIndex === fraseActual.length - 1 ? "green" : "gray";
+      }
+      animarTecla(letraActual);
+      setLetraActualIndex((prevIndex) => prevIndex + 1);
+      animationTimeout.current = setTimeout(teclearFrase, intervaloEntreLetras);
+    } else {
+      // Fin de la frase actual
+      fraseTimeout.current = setTimeout(() => {
+        setFraseActualIndex(() => Math.floor(Math.random() * frases.length));
+        setLetraActualIndex(0);
+        setTextoEscrito("");
+        // Reiniciar la animación para la siguiente frase
+        if (animationTimeout.current) {
+          clearTimeout(animationTimeout.current);
+          animationTimeout.current = null;
+        }
+        initialTimeoutRef.current = setTimeout(
+          teclearFrase,
+          intervaloEntreFrases
+        );
+      }, intervaloEntreFrases);
+    }
+  };
+
+  useEffect(() => {
+    // Iniciar la secuencia de tecleo después de un breve retraso inicial
+    initialTimeoutRef.current = setTimeout(teclearFrase, 500);
+
+    // Limpiar los timeouts cuando el componente se desmonta
+    return () => {
+      if (initialTimeoutRef.current) {
+        clearTimeout(initialTimeoutRef.current);
+      }
+      if (animationTimeout.current) {
+        clearTimeout(animationTimeout.current);
+      }
+      if (fraseTimeout.current) {
+        clearTimeout(fraseTimeout.current);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [frases]); // Depender de 'frases' asegura que se reinicie si las frases cambian
+
+  return (
+    <div
+      ref={tecladoContainerRef}
+      className="relative flex flex-col items-center"
+    >
+      <div
+        id="texto-escrito"
+        className="text-xl mb-5 p-2 h-12 border border-gray-300 rounded min-w-[300px] text-center"
+      >
+        {textoEscrito}
+      </div>
+      <div className="space-y-2">
+        {teclasData.map((fila, index) => (
+          <div
+            key={index}
+            className={`flex ${
+              index === teclasData.length - 1
+                ? "justify-start space-x-2"
+                : "space-x-2"
+            }`}
+          >
+            {fila.map((tecla) => (
+              <Tecla key={tecla} tecla={tecla} />
+            ))}
+          </div>
+        ))}
+      </div>
+      <EnterTecla />
+    </div>
+  );
+};
+
+export default LoadingTyping;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -12,6 +12,28 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        presionarTecla: {
+          "0%": {
+            transform: "translateY(0)",
+            boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "#eee",
+          },
+          "50%": {
+            transform: "translateY(2px)",
+            boxShadow: "1px 1px 2px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "#ddd",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "#eee",
+          },
+        },
+      },
+      animation: {
+        presionarTecla: "presionarTecla 0.3s ease-in-out",
       },
     },
   },
